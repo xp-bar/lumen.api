13c11ba88f6533663b5d52eed678b9f4593c5c92
Nicholas Ireland <nick@nicholasireland.ca>
2021-11-13 12:25:38 -0500

    Chore: upgrade lumen 7.x -> 8.x, fix factories, etc.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,5 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        //
     }
 }
